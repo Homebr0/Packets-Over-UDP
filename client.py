43868ac89fa9e25b0116b98d63dd8ee38a361967
Jason Davila <jsndavila1@gmail.com>
2020-03-05 12:36:33 -0500

    indentation fix

--- client.py
+++ client.py
@@ -34,11 +34,7 @@ while True:
         print("Receiving from socket")
         (inPacket, fromAddr) = sock.recvfrom(1024)
         # Note in the above, parameter to .recvfrom should be at least MTU+12 (524), but can be anything else larger if we are willing to accept larger packets
-<<<<<<< HEAD
-        print ("prcessing packet")
-=======
         print ("processing packet")
->>>>>>> eddb9dc2a9df655533a3de5bc0f78eb2a2af00d5
         # Process incoming packet
         conn.on_receive(inPacket)
         print("processing retransmission")
