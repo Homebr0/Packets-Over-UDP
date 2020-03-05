537b8b83537688d84aa53640d249e10c578b8073
Jason Davila <jsndavila1@gmail.com>
2020-03-05 16:46:49 -0500

    I added a file /makedata.py/ which creates a file of any specified size consisting of random chars. Also included a HUGE 500MiB file and a 100MiB file for testing

--- client.py
+++ client.py
@@ -34,10 +34,10 @@ while True:
         print("Receiving from socket")
         (inPacket, fromAddr) = sock.recvfrom(1024)
         # Note in the above, parameter to .recvfrom should be at least MTU+12 (524), but can be anything else larger if we are willing to accept larger packets
-        print ("processing packet")
+        
         # Process incoming packet
         conn.on_receive(inPacket)
-        print("processing retransmission")
+        
         # Process any retransmissions
         conn.process_retransmissions()
 
@@ -57,7 +57,7 @@ while True:
         if not data:
             file = None
             break
-        print("sending file data")
+        print(data)
         conn.send(data)
 
     if not file and conn.canSendData():
