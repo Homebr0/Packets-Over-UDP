a42dc48e6f2afc7adf2bc10b5bd5bbedc6fd93de
Issachar Vinajeras <issacharv@gmail.com>
2020-03-07 13:52:54 -0500

    initial commit

--- confundo/ostream.py
+++ confundo/ostream.py
@@ -8,7 +8,6 @@ from enum import Enum
 import time
 import sys
 
-
 class State(Enum):
     INVALID = 0
     SYN = 1
@@ -18,36 +17,31 @@ class State(Enum):
     CLOSED = 20
     ERROR = 21
 
-
 class Ostream:
-    def __init__(self, base=12345, isOpening=True):
+    def __init__(self, base = 12345, isOpening = True):
         self.base = base
         self.seqNum = base
-        self.lastAckTime = time.time()  # last time ACK was sent / activity timer
+        self.lastAckTime = time.time() # last time ACK was sent / activity timer
         self.cc = CwndControl()
         self.buf = b""
         self.state = State.INVALID
         self.nDupAcks = 0
-        self.ackNum = 0
 
     def ack(self, ackNo, connId):
         if self.state == State.INVALID:
-
-            print ("state is invalid")
-
             return None
 
-        self.lastAckTime = time.time()
+        ###
+        ### IMPLEMENT
+        ###
         pass
 
-    def makeNextPacket(self, connId, payload, isSyn=False, isFin=False, isAck = False, **kwargs):
-        if self.seqNum == MAX_SEQNO:
-            self.seqNum = 0
-        pkt = Packet(seqNum=self.seqNum, ackNum = self.ackNum, connId=connId, isSyn=isSyn, isFin=isFin, payload=payload, isAck = isAck)        
-        #self.seqNum += 1        
-        self.state = State.OPEN
-        return pkt
-        
+    def makeNextPacket(self, connId, payload, isSyn=False, isFin=False, **kwargs):
+        ###
+        ### IMPLEMENT
+        ###
+        pass
+
     def hasBufferedData(self):
         ###
         ### IMPLEMENT
@@ -61,25 +55,22 @@ class Ostream:
         pass
 
     def on_timeout(self, connId):
-        if 10 <= (time.time() - self.lastAckTime):
-            return True
+        ###
+        ### IMPLEMENT
+        ###
         return None
 
     def canSendData(self):
-        if self.state == State.OPEN:
-            return True
-        else:
-            return False
+        ###
+        ### IMPLEMENT
+        ###
+        pass
 
-    def canSendNewData(self, cwnd_control, congestionLength):
-        self.cwnd_control = cwnd_control
-        self.congestionLength = congestionLength
-        #Send only if state is open and cwnd is less than the actual congestion length
-        if self.state == State.OPEN and (self.cwnd_control.cwnd > self.congestionLength):
-            return True
-        else:
-            return False
-
+    def canSendNewData(self):
+        ###
+        ### IMPLEMENT
+        ###
+        pass
 
     def __str__(self):
         return f"state:{self.state} base:{self.base} seqNum:{self.seqNum} nSentData:{len(self.buf)} cc:{self.cc}"
